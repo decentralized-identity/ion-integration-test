e4df041f6e3a3b658d04017fd474579d02d76929
isaacJChen <isaac.jeffery.chen@gmail.com>
2020-12-22 14:04:58 -0500

    ideas

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
+// Import the config which contains what tests to run
+// Execute them with desired retry interval (blockchain anchor time) and timeout (after how long should the test be a failure)
+
 console.log('Hello world');
