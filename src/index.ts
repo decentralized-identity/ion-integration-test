52eaa662c211b988d257af3721deea5b4c038344
isaacJChen <isaac.jeffery.chen@gmail.com>
2020-12-22 14:23:08 -0500

    update comment

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // Import the config which contains what tests to run
-// Execute them with desired retry interval (blockchain anchor time) and timeout (after how long should the test be a failure)
+// Execute them with desired retry interval and timeout (expected blochchain anchor time and after how long should the test be a failure)
 
 // Current design only tests the happy paths
 
